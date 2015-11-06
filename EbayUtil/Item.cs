1e01c4cbebf6b4bd046bfa543f35ef5be38643c2
Sarim Javaid Khan <sarim.javaid.khan@gmail.com>
2015-11-06 12:47:54 +0500

    First Push

---
+++ EbayUtil/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbayUtil
+{
+	public class Item
+	{
+		public string email { set; get; }
+	}
+}
---
+++ EbayUtil/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace EbayUtil
+{
+	class Program
+	{
+		public static HtmlDocument doc;
+		public static HtmlWeb web;
+	
+		public static string allCatsUrl = "http://www.ebay.co.uk/sch/allcategories/all-categories";
+		public static List<string> catUrls;
+
+		public static List<Item> sellers;
+
+		//1
+		public static void initializeVars()
+		{
+			doc = new HtmlDocument();
+			web = new HtmlWeb();
+
+			catUrls = new List<string>();
+
+			sellers = new List<Item>();
+		}
+
+		//2
+		public static void loadCategoriesPage()
+		{
+			doc = web.Load(allCatsUrl);
+		}
+
+		//3
+		public static void loadCategoryUrls(string xpath)
+		{
+			List<HtmlNode> sellerNodes = new List<HtmlNode>();
+
+			sellerNodes = doc.DocumentNode.SelectNodes(xpath).ToList<HtmlNode>();
+		}
+
+		//0
+		static void Main(string[] args)
+		{
+			initializeVars();
+			loadCategoriesPage();
+			Console.Read();
+		}
+	}
+}
